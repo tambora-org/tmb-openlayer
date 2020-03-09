14bba82c3c53f11f7c796980858062d99b3532e8
Unknown <michael.kahle@yahoo.de>
2020-03-09 17:23:25 +0100

    Initial openlayer layer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+
+class TamboraMarkerLayer extends ol.layer.Vector {
+   constructor(options) {
+	   super(options);
+       // this.year = options.year;
+   }
+}   
+
+
+//class ol.layer.TamboraMarkerLayer {
+//   constructor(options) {
+//	   this = new  ol.layer.Vector(options);
+//	   //super(options);
+//      // this.year = options.year;
+//}
+//} 
+
+
+  // Style function
+  function getFeatureStyle (feature) {
+    var st= [];
+    
+
+    // Font style
+    st.push ( new ol.style.Style({
+      image: new ol.style.FontSymbol({
+          form: "marker", 
+          gradient: false,
+          glyph: "fa-info", 
+          fontSize: 0.9,
+          fontStyle: 'unset',
+          radius: 15, 
+          //offsetX: -15,
+          rotation: 0,
+          rotateWithView: true,
+          offsetY: 0 ,
+          color: 'red',
+          fill: new ol.style.Fill({
+            color: 'blue'
+          }),
+          stroke: new ol.style.Stroke({
+            color: 'black',
+            width: 2
+          })
+        }),
+        stroke: new ol.style.Stroke({
+          width: 2,
+          color: '#f80'
+        }),
+        fill: new ol.style.Fill({
+          color: [255, 136, 0, 0.6]
+        })
+      }));
+      return st;
+  }
+
+  function getStyle(feature, resolution) {
+    var s = getFeatureStyle(feature);
+    // Ne pas recalculer
+    //feature.setStyle(s);
+    return s;
+    
+  };
